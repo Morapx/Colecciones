43a8c2d6ed17b8040a618a4f08350bb42ed9d0f3
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-23 08:48:16 -0700

    Agregar archivos de proyecto.

---
+++ Colecciones/Grupo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Grupo
+    {
+        public string Materia { get; set; }
+        public string Codigo { get; set; }
+        public List<Alumno> Alumno { get; set; }
+
+        public Grupo(string materia, string codigo)
+            {
+            Materia = materia;
+            Codigo = codigo;
+            Alumno = new List<Alumno>();
+        }
+    }
+}
---
+++ Colecciones/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Colecciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Grupo> grupos = new List<Grupo>();
+
+            Grupo matematicas = new Grupo("Matematicas", "MAT5736");
+            Grupo historia = new Grupo("Historia", "HST2149");
+
+            grupos.Add(matematicas);
+            grupos.Add(historia);
+
+            Alumno juan = new Alumno("Juan", "78924");
+            Alumno maria = new Alumno("Maria", "15478");
+            Alumno pedro = new Alumno("Pedro", "41544");
+            Alumno ana = new Alumno("ana", "52698");
+            Alumno pablo = new Alumno("Pablo", "24578");
+
+            historia.Alumno.Add(juan);
+            historia.Alumno.Add(ana);
+            historia.Alumno.Add(pedro);
+
+            matematicas.Alumno.Add(juan);
+            matematicas.Alumno.Add(maria);
+            matematicas.Alumno.Add(pedro);
+            matematicas.Alumno.Add(ana);
+            matematicas.Alumno.Add(pablo);
+
+            foreach(Grupo grupo in grupos)
+            {
+                Console.WriteLine("Grupos" + grupo.Codigo);
+                Console.WriteLine("Materia" + grupo.Materia);
+                Console.WriteLine("Lista de alumnos");
+                foreach (Alumno alumno in grupo.Alumno)
+                {
+                    Console.WriteLine("*" + alumno.Nombre);
+                }
+            }
+
+            Console.Read();
+        }  
+    }
+}
